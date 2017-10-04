715380620e4d1d9d207bb27b65a64a9ab632a2d1
Olof Almqvist <a15oloal@hs.local>
2017-10-04 15:15:09 +0200

    Loggtabell & trigger

--- PUCKO.sql
+++ PUCKO.sql
@@ -53,10 +53,6 @@ create table Hjälpmedel(
     foreign key(Typkod, Ordningsnummer) references Hjälpmedelstyper(Typkod, Ordningsnummer)
 )engine=innodb;
 
-
-#Using Concrete Table Inheritance martinFowler.com
-# https://martinfowler.com/eaaCatalog/concreteTableInheritance.html
-#Kan inte hantera partiell nedärvning och kan bli oflexibelt, men ger bäst prestanda när sökningar görs. Förväntning: total nedärvning.
 create table Gruppledare(
 	Namn char(1),
     Nr tinyint(2),
@@ -124,54 +120,71 @@ END;
 //
 DELIMITER ;
 
-create table Operation(
+create table Operation_Pågående(
     Kodnamn char(3),
     Operationstyp varchar(40),
     Startdatum date,
-    Incidentnamn varchar(30),
     Slutdatum date,
-    Sucess_Rate tinyint(1),
+    Incidentnamn varchar(30),
     Gruppledarnamn char(1),
     Gruppledarnr tinyint(2),
+    Hjälpmedelnamn varchar(20),
+    Hjälpmedelnr smallint,
+    Fältagentnamn char(1),
+    Fältagentnr tinyint(2),
     CHECK (Kodnamn LIKE '[A-Z][0-9][0-9]'),
     CHECK ((DAY(Slutdatum) > DAY(Startdatum) AND (YEAR(Slutdatum) = YEAR(Startdatum))) OR (YEAR(Slutdatum) > YEAR(Startdatum))),
-    primary key(Kodnamn, Operationstyp, Startdatum, Incidentnamn),
+    primary key(Kodnamn, Operationstyp, Startdatum, Incidentnamn, Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr),
     foreign key(Incidentnamn) references Incident(Namn),
+    foreign key(Hjälpmedelnamn, Hjälpmedelnr) references Hjälpmedel(Namn, Nr),
+    foreign key(Fältagentnamn, Fältagentnr) references Fältagent(Namn, Nr),
     foreign key (Gruppledarnamn, Gruppledarnr) references Gruppledare(Namn, Nr)
 )engine=innodb;
 
-
-/* # INDEX FOR QUERY SELECT * FROM Operation ORDER BY Slutdatum; 
-CREATE INDEX Operationer_Slutdatum ON Operation(Slutdatum ASC) USING BTREE; */
-
-/*
-# Brytit upp Operationer till Operationstyper för att minska redundans
-create table Operationstyper(
-	Kodnamntyp char(3),
-    Operationstyp varchar(40) NOT NULL,
-    primary key (Kodnamntyp),
-    foreign key (Kodnamntyp) references Operation(Kodnamntyp)
-)engine=innodb;
-*/
-
-create table Operationers_hjälpmedel(
-	Hjälpmedelnamn varchar(20),
-    Hjälpmedelnr smallint,
+create table Operation_Avslutad(
     Kodnamn char(3),
     Operationstyp varchar(40),
     Startdatum date,
+    Slutdatum date,
     Incidentnamn varchar(30),
-    primary key(Hjälpmedelnamn, Hjälpmedelnr, Kodnamn, Operationstyp, Startdatum, Incidentnamn),
+    Sucess_Rate tinyint(1),
+    Gruppledarnamn char(1),
+    Gruppledarnr tinyint(2),
+    Hjälpmedelnamn varchar(20),
+    Hjälpmedelnr smallint,
+    Fältagentnamn char(1),
+    Fältagentnr tinyint(2),
+    CHECK (Kodnamn LIKE '[A-Z][0-9][0-9]'),
+    CHECK ((DAY(Slutdatum) > DAY(Startdatum) AND (YEAR(Slutdatum) = YEAR(Startdatum))) OR (YEAR(Slutdatum) > YEAR(Startdatum))),
+    primary key(Kodnamn, Operationstyp, Startdatum, Incidentnamn, Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr),
+    foreign key(Incidentnamn) references Incident(Namn),
     foreign key(Hjälpmedelnamn, Hjälpmedelnr) references Hjälpmedel(Namn, Nr),
-    foreign key(Kodnamn, Operationstyp, Startdatum, Incidentnamn) references Operation(Kodnamn, Operationstyp, Startdatum, Incidentnamn)
+    foreign key(Fältagentnamn, Fältagentnr) references Fältagent(Namn, Nr),
+    foreign key (Gruppledarnamn, Gruppledarnr) references Gruppledare(Namn, Nr)
 )engine=innodb;
-
+  
+-- INDEX FOR QUERY SELECT * FROM Operation_Avslutad ORDER BY Slutdatum; 
+CREATE INDEX Operationer_Slutdatum ON Operation_Avslutad(Slutdatum ASC) USING BTREE;
 
 
 /* Trigger för att hantera om start- och slutdatum är mindre än 5 veckor mellan varandra */
 DELIMITER //
  
-CREATE TRIGGER DATUMCHECK_Operation BEFORE INSERT ON Operation
+CREATE TRIGGER DATUMCHECK_Operation_Pågående BEFORE INSERT ON Operation_Pågående
+FOR EACH ROW BEGIN
+ 
+  IF((WEEK(NEW.Slutdatum) - WEEK(NEW.Startdatum)) > 5 ) THEN
+    SET NEW.Slutdatum = (DATE_ADD(NEW.Startdatum, INTERVAL 5 WEEK));
+  END IF;
+ 
+END;
+ 
+//
+DELIMITER ;
+
+DELIMITER //
+ 
+CREATE TRIGGER DATUMCHECK_Operation_Avslutad BEFORE INSERT ON Operation_Avslutad
 FOR EACH ROW BEGIN
  
   IF((WEEK(NEW.Slutdatum) - WEEK(NEW.Startdatum)) > 5 ) THEN
@@ -193,25 +206,11 @@ create table Fältagenters_hjälpmedel(
     foreign key(Fältagentnamn, Fältagentnr) references Fältagent(Namn, Nr)
 )engine=innodb;
 
-create table Fältagenter_operationer(
-    Fältagentnamn char(1),
-    Fältagentnr tinyint(2),
-    Kodnamn char(3),
-    Operationstyp varchar(40),
-    Startdatum date,
-    Incidentnamn varchar(30),
-    primary key(Fältagentnamn, Fältagentnr, Kodnamn, Operationstyp, Startdatum, Incidentnamn),
-    foreign key(Fältagentnamn, Fältagentnr) references Fältagent(Namn, Nr), 
-    foreign key(Kodnamn, Operationstyp, Startdatum, Incidentnamn) references Operation(Kodnamn, Operationstyp, Startdatum, Incidentnamn)
-)engine=innodb;
-
 create table Slutrapport(
     Datum date,
     Titel varchar(30),
-    n_uppföljningar int,
     n_rader int,
     Kommentar varchar(25),
-    Typ varchar(16),
     Fältagentnamn char(1),
     Fältagentnr tinyint(2),
     Gruppledarnamn char(1),
@@ -225,29 +224,95 @@ create table Slutrapport(
     foreign key (Incidentnamn) references Incident(Namn)
 )engine=innodb;
 
+/*Tabellen som lagrar loggar om incidentinsättningar*/
+create table Incident_logg(
+	Anvandarnamn varchar(25),
+    Tid datetime,
+	Namn varchar(30),
+    Nr int UNIQUE NOT NULL,
+    Plats varchar(30) NOT NULL,
+    primary key(Namn)
+)engine=innodb;
+
+insert into Gruppledare (Namn, Nr, Lön, Förnamn, Efternamn) values ('A', 12, 26000, 'Richard', 'Lionmane');
+insert into Fältagent (Namn, Nr, Lön, Förnamn, Efternamn, Specialite, Kompetens) values ('D', 18, 15500, 'Siegfried', 'Schultz', 'Krypskytte', 'Hög');
+insert into Hjälpmedelstyper (Typkod, Ordningsnummer, Typ) values (5566, 1, 'Långdistansgevär');
+insert into Hjälpmedel (Namn, Nr, Beskrivning, Typkod, Ordningsnummer) values ('AXD Sniper', 423, 'Amerikansk modell', 5566, 1);
+insert into Fältagenters_hjälpmedel (Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr) values ('AXD Sniper', 423, 'D', 18);
 insert into Incident (Namn, Nr, Plats) values ('Ulvahändelsen', 5, 'Töreboda');
-insert into Operation (Startdatum, Slutdatum, Incidentnamn) values ('2017-01-01', '2017-05-05', 'Ulvahändelsen');
-select * from Operation;
+insert into Operation_Pågående (Startdatum, Slutdatum, Incidentnamn, Gruppledarnamn, Gruppledarnr, Operationstyp, Kodnamn, Fältagentnamn, Fältagentnr, Hjälpmedelnamn, Hjälpmedelnr) values ('2017-01-01', '2017-05-05', 'Ulvahändelsen', 'A', 12, 'Uppstädningsoperation', 'ABD', 'D', 18, 'AXD Sniper', 423);
+
+insert into Gruppledare (Namn, Nr, Lön, Förnamn, Efternamn) values ('B', 2, 29000, 'Peter', 'Bass');
+insert into Fältagent (Namn, Nr, Lön, Förnamn, Efternamn, Specialite, Kompetens) values ('C', 4, 16000, 'Gottfried', 'Stark', 'Krypskytte', 'Låg');
+insert into Hjälpmedelstyper (Typkod, Ordningsnummer, Typ) values (5566, 2, 'Långdistansgevär');
+insert into Hjälpmedel (Namn, Nr, Beskrivning, Typkod, Ordningsnummer) values ('AXD Sniper', 425, 'Rysk modell', 5566, 2);
+insert into Fältagenters_hjälpmedel (Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr) values ('AXD Sniper', 425, 'C', 4);
+insert into Operation_Pågående (Operationstyp, Startdatum, Slutdatum, Incidentnamn, Gruppledarnamn, Gruppledarnr, Kodnamn, Fältagentnamn, Fältagentnr, Hjälpmedelnamn, Hjälpmedelnr) values ('Uppstädningsoperation', '2017-01-01', '2017-02-05', 'Ulvahändelsen', 'B', 2, 'ABC', 'C', 4, 'AXD Sniper', 425);
+
+insert into Gruppledare (Namn, Nr, Lön, Förnamn, Efternamn) values ('T', 8, 8000, 'Wolfgang', 'Vlk');
+insert into Fältagent (Namn, Nr, Lön, Förnamn, Efternamn, Specialite, Kompetens) values ('R', 6, 5000, 'Frida', 'Ekdahl', 'Manipulation', 'Medel');
+insert into Hjälpmedelstyper (Typkod, Ordningsnummer, Typ) values (23, 1, 'Sanningsserum');
+insert into Hjälpmedel (Namn, Nr, Beskrivning, Typkod, Ordningsnummer) values ('Klarvätska Pfizer', 156, 'Prövades under Ulvahändelsen', 23, 1);
+insert into Hjälpmedelstyper (Typkod, Ordningsnummer, Typ) values (120, 1, 'Glock');
+insert into Hjälpmedel (Namn, Nr, Beskrivning, Typkod, Ordningsnummer) values ('Pistol 88', 1010, 'Standardpistol', 120, 1);
+insert into Fältagenters_hjälpmedel (Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr) values ('Klarvätska Pfizer', 156, 'R', 6);
+insert into Fältagenters_hjälpmedel (Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr) values ('Pistol 88', 1010, 'R', 6);
+insert into Incident (Namn, Nr, Plats) values ('Läcköläckan', 1888, 'Lidköping');
+insert into Operation_Avslutad (Operationstyp, Startdatum, Slutdatum, Incidentnamn, Gruppledarnamn, Gruppledarnr, Kodnamn, Fältagentnamn, Fältagentnr, Hjälpmedelnamn, Hjälpmedelnr, Sucess_Rate) values ('Desinformationsuppdrag', '2011-06-22', '2011-06-29', 'Läcköläckan', 'T', 8, 'DRR', 'R', 6, 'Klarvätska Pfizer', 156, 1);
+insert into Operation_Avslutad (Operationstyp, Startdatum, Slutdatum, Incidentnamn, Gruppledarnamn, Gruppledarnr, Kodnamn, Fältagentnamn, Fältagentnr, Hjälpmedelnamn, Hjälpmedelnr, Sucess_Rate) values ('Desinformationsuppdrag', '2011-06-22', '2011-06-29', 'Läcköläckan', 'T', 8, 'DRR', 'R', 6, 'Pistol 88', 1010, 1);
+
+insert into Slutrapport (Datum, Titel, n_rader, Kommentar, Fältagentnamn, Fältagentnr, Gruppledarnamn, Gruppledarnr, Incidentnamn) values ('2011-06-29', 'Läcköchefen förhörd', 10, 'All info erhölls', 'R', 6, 'T', 8, 'Läcköläckan');
+
+insert into Gruppledare (Namn, Nr, Lön, Förnamn, Efternamn) values ('E', 14, 29550, 'Hermann', 'Brandt');
+insert into Fältagent (Namn, Nr, Lön, Förnamn, Efternamn, Specialite, Kompetens) values ('G', 89, 22300, 'Freja', 'Stenhammar', 'Strategisk analys', 'Hög');
+insert into Fältagent (Namn, Nr, Lön, Förnamn, Efternamn, Specialite, Kompetens) values ('G', 88, 20100, 'Sven', 'Persson', 'Personskydd', 'Medel');
+insert into Hjälpmedelstyper (Typkod, Ordningsnummer, Typ) values (45, 1, 'Satelitdator');
+insert into Hjälpmedel (Namn, Nr, Beskrivning, Typkod, Ordningsnummer) values ('Satelitdator', 189, 'Oberoende av marknät', 45, 1);
+insert into Hjälpmedelstyper (Typkod, Ordningsnummer, Typ) values (120, 2, 'Glock');
+insert into Hjälpmedel (Namn, Nr, Beskrivning, Typkod, Ordningsnummer) values ('Pistol 88', 1011, 'Standardpistol', 120, 2);
+insert into Fältagenters_hjälpmedel (Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr) values ('Satelitdator', 189, 'G', 89);
+insert into Fältagenters_hjälpmedel (Hjälpmedelnamn, Hjälpmedelnr, Fältagentnamn, Fältagentnr) values ('Pistol 88', 1011, 'G', 88);
+insert into Incident (Namn, Nr, Plats) values ('Annunakimötet', 1566, 'Kungsbacka');
+insert into Operation_Avslutad (Operationstyp, Startdatum, Slutdatum, Incidentnamn, Gruppledarnamn, Gruppledarnr, Kodnamn, Fältagentnamn, Fältagentnr, Hjälpmedelnamn, Hjälpmedelnr, Sucess_Rate) values ('Diplomatiskt möte', '2016-07-15', '2016-07-16', 'Annunakimötet', 'E', 14, 'TRI', 'G', 89, 'Satelitdator', 189, 1);
+insert into Operation_Avslutad (Operationstyp, Startdatum, Slutdatum, Incidentnamn, Gruppledarnamn, Gruppledarnr, Kodnamn, Fältagentnamn, Fältagentnr, Hjälpmedelnamn, Hjälpmedelnr, Sucess_Rate) values ('Diplomatiskt möte', '2016-07-15', '2016-07-16', 'Annunakimötet', 'E', 14, 'TRI', 'G', 88, 'Pistol 88', 1011, 1);
+
+insert into Slutrapport (Datum, Titel, n_rader, Kommentar, Fältagentnamn, Fältagentnr, Gruppledarnamn, Gruppledarnr, Incidentnamn) values ('2011-07-17', 'Möte med Annunaki', 20, 'Inga fler besök', 'G', 89, 'E', 14, 'Annunakimötet');
+
+/*
+select * from Gruppledare;
+select * from Fältagent;
+select * from Incident;
+select * from Incident_logg;
+select * from Hjälpmedelstyper;
+SELECT * FROM Hjälpmedel;
+SELECT * FROM Fältagenters_hjälpmedel;
+SELECT * FROM Operation_Pågående;
+SELECT * FROM Operation_Avslutad;
+SELECT * FROM Slutrapport;
+*/
 
 /* CREATING USER RIGHTS FOR THE SYSTEM */
 
 /*** USER GRUPPLEDARE ***/
 # Create a user for the gruppledare application
+# drop user 'gruppledare';
+flush privileges;
 CREATE USER 'gruppledare'@'localhost' IDENTIFIED BY 'password';
  
-# Gives select & update access to Fältagent & Fältagenter_operationer tables to gruppledare
+# Gives select & update access to Fältagent table to gruppledare
 GRANT SELECT, UPDATE ON a15oloal.Fältagent TO gruppledare;
-GRANT SELECT, UPDATE ON a15oloal.Fältagenter_operationer TO gruppledare;
 
 # Gives select & update, insert access to Operation, Slutrapport & Incident tables to gruppledare
-GRANT SELECT, UPDATE, INSERT ON a15oloal.Operation TO gruppledare;
+GRANT SELECT, UPDATE, INSERT ON a15oloal.Operation_Pågående TO gruppledare;
+GRANT SELECT, UPDATE, INSERT ON a15oloal.Operation_Avslutad TO gruppledare;
 GRANT SELECT, UPDATE, INSERT ON a15oloal.Incident TO gruppledare;
 GRANT SELECT, INSERT ON a15oloal.Slutrapport TO gruppledare;
 
 # Gives select & update, insert, delete access to Hjälpmedel, operationers_hjälpmedel & Fältagenters_hjälpmedel tables to gruppledare
 GRANT SELECT, UPDATE, INSERT ON a15oloal.Hjälpmedel TO gruppledare;
+GRANT SELECT, UPDATE, INSERT ON a15oloal.Hjälpmedelstyper TO gruppledare;
 GRANT SELECT, UPDATE, INSERT ON a15oloal.Fältagenters_hjälpmedel TO gruppledare;
-GRANT SELECT, UPDATE, INSERT ON a15oloal.operationers_hjälpmedel TO gruppledare;
+
  
 # Create a view that permits access to gruppledare.namn
 CREATE VIEW GRUPPLEDARE AS SELECT Namn, lyckade_operationer, n_operationer, andel_lyckade_op FROM Gruppledare;
@@ -260,24 +325,39 @@ GRANT SELECT ON a15oloal.GRUPPLEDARE to gruppledare;
 CREATE USER 'fältagent'@'localhost' IDENTIFIED BY 'password';
  
  # Create a view that permits access to gruppledare.namn
-CREATE VIEW FÄLT_GRUPPLEDARE AS SELECT Namn FROM Gruppledare; 
+CREATE VIEW GRUPPLEDARNAMN AS SELECT Namn FROM Gruppledare; 
 # Gives select access to FÄLT_GRUPPLEDARE VIEW to Fältagent
-GRANT SELECT ON a15oloal.FÄLT_GRUPPLEDARE TO fältagent;
+GRANT SELECT ON a15oloal.GRUPPLEDARNAMN TO fältagent;
 
  # Create a view that permits access to historic operations
 CREATE VIEW FÄLT_OPERATION AS SELECT Kodnamn, Operationstyp, Startdatum, Incidentnamn, Slutdatum, Sucess_Rate, Gruppledarnamn 
-FROM Operation WHERE Sucess_Rate = 0 OR Sucess_Rate = 1;
+FROM Operation_Avslutad;
 # Gives select access to FÄLT_OPERATION VIEW to Fältagent
 GRANT SELECT ON a15oloal.FÄLT_OPERATION TO fältagent;
 
-# Gives select access to Incident table to Fältagent
-GRANT SELECT ON a15oloal.Incident TO fältagent;
+# Create a view of historic incidents
+CREATE VIEW 
+	HISTORISKA_INCIDENTER
+AS
+	SELECT(Namn, Incidentnamn)
+    FROM Incident, Operation_Avslutad
+    WHERE Incident.Namn = Operation_Avslutad.Incidentnamn;
+
+# Grant all privileges on the view to fältagent user
+GRANT ALL ON HISTORISKA_INCIDENTER TO fältagent;
 
 # Gives access to insert and select Slutrapport table to Fältagent
 GRANT SELECT, INSERT ON a15oloal.Slutrapport TO fältagent;
 
-# Gives select access to Fältagenters_operationer table to Fältagent
-CREATE VIEW TEAM_OPERATION AS SELECT * ON a15oloal.Fältagenters_hjälpmedel TO fältagent
-WHERE Fältagenters_hjälpmedel.Fältagentnamn = user() AND Fältagenters_hjälpmedel.Fältagentnr = user();
+# Gives select access to Fältagenters_operationer table to Fältagent 						 Hur ska jag skapa den här lösningen?
+/*CREATE VIEW TEAM_OPERATION AS SELECT * ON a15oloal.Fältagenters_hjälpmedel TO fältagent
+WHERE Fältagenters_hjälpmedel.Fältagentnamn = user() AND Fältagenters_hjälpmedel.Fältagentnr = user(); */
 
-GRANT SELECT ON TEAM_OPERATION TO fältagent
+/*Loggar nya incidenter*/
+DELIMITER //
+CREATE TRIGGER Incident_Logg AFTER INSERT ON Incident
+FOR EACH ROW BEGIN 
+   INSERT INTO Incident_logg(Anvandarnamn,Tid,Incidentnamn,Incidentnr,Plats) 
+      values(USER(),NOW(),NEW.Incidentnamn,NEW.Incidentnr,NEW.Plats);
+END;
+// DELIMITER ;
